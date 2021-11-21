e49875e25aadc30b7278cf3031faa6e6f9c0d556
Nicolás Montiel <nicolasmontielf@gmail.com>
2021-11-21 17:13:26 -0300

    Server config for express

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,27 @@ import logger from "morgan";
 import * as path from "path";
 import dotenv from "dotenv"
 import connectToMongoDB from "./database/connection"
+import response from "./middlewares/response";
+import router from "./routes";
 
 // Initialize dotenv and database connection
 dotenv.config()
 connectToMongoDB()
 
-// Routes
-import { index } from "./routes/index";
-
 // Create Express server
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middlewares 
 app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json())
+app.use(response)
 
-app.use("/", index);
+// Routes
+app.use("/api", router);
 
+// Events
 app.listen(port, onListening);
 app.on("error", onError);
 
